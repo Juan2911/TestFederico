bc60c085fee5ead38dadcecc904115023c4d88f5
Juan Jose Guzman <juan_ratablank@hotmail.com>
2022-02-25 18:18:27 -0300

    initial commit

---
+++ src/main/java/com/Medisoft/Medisoft/Services/impl/PersonaServiceImpl.java
@@ -0,0 +1,14 @@
+package com.Medisoft.Medisoft.Services.impl;
+
+import com.Medisoft.Medisoft.Domain.Persona;
+import com.Medisoft.Medisoft.Services.PersonaService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaServiceImpl implements PersonaService {
+
+
+    @Override
+    public void postPersona(Persona persona) {
+    }
+}
